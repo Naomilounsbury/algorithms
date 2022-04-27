6b8a91a762e3939104ba4e586dc39ce7a91b5aad
naomi <naomilounsbury@hotmail.com>
2022-04-26 20:15:08 -0700

    yet another one, I freaken win

--- 2/01-log-even-nums/Unsolved/log-even-nums.js
+++ 2/01-log-even-nums/Unsolved/log-even-nums.js
@@ -1,4 +1,11 @@
 // Write code to print all even numbers from 0 to `num`
 // Assume `num` will be a positive number
 
-var logEvenNums = function(num) {};
+var logEvenNums = function (num) {
+  //I want an if statement inside a for loop because for every variable if a number before that variable has nothing left over after being divided by 2 then I want to console log all those numbers
+  for (var i = 0; i <= num; i++) {
+    if (i % 2 === 0) {
+      console.log(i);
+    }
+  }
+};
