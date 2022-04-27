5e4615add4a9c639607eee5fbf3d661a1ed7b2dd
naomi <naomilounsbury@hotmail.com>
2022-04-26 20:38:57 -0700

    finished countdown

--- 2/01-log-even-nums/Unsolved/log-even-nums.js
+++ 2/01-log-even-nums/Unsolved/log-even-nums.js
@@ -2,8 +2,9 @@
 // Assume `num` will be a positive number
 
 var logEvenNums = function (num) {
-  //I want an if statement inside a for loop because for every variable if a number before that variable has nothing left over after being divided by 2 then I want to console log all those numbers
+  //I want an if statement inside a for loop because for every variable if a number before that num has nothing left over after being divided by 2 then I want to console log all those numbers
   for (var i = 0; i <= num; i++) {
+      //its not num%2 because we don't want to see if num is we want to check all the numbers before num and push them through our for loop and then log them
     if (i % 2 === 0) {
       console.log(i);
     }
--- 2/02-countdown/Unsolved/countdown.js
+++ 2/02-countdown/Unsolved/countdown.js
@@ -1,4 +1,9 @@
 // Write code to print all numbers from `num` down to 1
 // You may assume `num` will be a positive number
 
-var countdown = function(num) {};
+var countdown = function (num) {
+    //for the variable i is placed at whatever num to start and then as long as i is greater than 0 then it will show the countdown of those numbers. Its a reverse for loop
+  for (var i = num; i > 0; i--) {
+    console.log(i);
+  }
+};
