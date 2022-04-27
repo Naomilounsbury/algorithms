9a7538cbcee13bc064788d35a11a78d225b2722e
naomi <naomilounsbury@hotmail.com>
2022-04-26 18:02:13 -0700

    haven't yet figured out how to test my code but I figure I'll get there

--- 1/03-log-nums/Unsolved/log-nums.js
+++ 1/03-log-nums/Unsolved/log-nums.js
@@ -1,4 +1,15 @@
 // Write code to print all numbers from 1 to `num`
 // Assume `num` will be a positive number
 
-var logNums = function(num) {};
+var logNums = function (num) {
+  if (num === 1) {
+    return 1;
+  }
+  if (num < 1) {
+    return 0;
+  }
+
+  for (var i = 1; i < num.length - 1; i++) {
+    console.log(i);
+  }
+};
