989caccf5d209f37e31ad20f815fd1d64a49133a
naomi <naomilounsbury@hotmail.com>
2022-05-05 13:49:02 -0700

    vowel count

--- 3/03-vowel-count/Unsolved/vowel-count.js
+++ 3/03-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,17 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  const vowelArr = ["a", "e", "i", "o", "u"];
+  let count = 0;
+  const splitString = str.toLowerCase().split("");
+  //   if(splitString.length<1){
+  //       return 0
+  //   }
+  console.log(splitString);
+  splitString.forEach((letter) => {
+    if (vowelArr.includes(letter)) {
+      count++;
+    }
+  });
+  return count;
+};
