4d8915d2d808be16f0c3bd4b3fa7fdee96114c87
naomi <naomilounsbury@hotmail.com>
2022-05-06 14:00:37 -0700

    palindrome

--- 4/01-is-palindrome/Unsolved/is-palindrome.js
+++ 4/01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,19 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function (str) {
+  console.log(str.split(""));
+  const splitStr = str.toLowerCase().split("");
+  console.log(splitStr.indexOf(splitStr[1]), "lalalaalal");
+
+  for (var i = 0; i < splitStr.length; i++) {
+    const comparativeLetter = str.charAt(
+      splitStr.length - 1 - splitStr.indexOf(splitStr[i])
+    );
+    console.log(splitStr[i], "the original letter");
+    console.log(comparativeLetter, "the comparative letter");
+    if (splitStr[i] !== comparativeLetter) {
+      return false;
+    }
+  }
+  return true;
+};
