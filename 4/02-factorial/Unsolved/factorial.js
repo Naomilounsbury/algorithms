b8a5cb8b52e99bccbe5f3ca73d5c2a50ccdebb1f
naomi <naomilounsbury@hotmail.com>
2022-05-06 14:27:12 -0700

    factorial, but i made it more complicated than it had to be

--- 4/02-factorial/Unsolved/factorial.js
+++ 4/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,13 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function (num) {
+  if (num === 0) {
+    return 1;
+  }
+  const arr = Array(num).fill(0);
+  return arr
+    .map((value, index) => {
+      return num - index;
+    })
+    .reduce((a, b) => a * b);
+};
