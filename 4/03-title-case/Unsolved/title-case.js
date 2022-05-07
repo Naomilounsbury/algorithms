d4b1e24bc29517461f1769c69bc5e177ad7f3f02
naomi <naomilounsbury@hotmail.com>
2022-05-07 13:34:13 -0700

    title-case

--- 4/03-title-case/Unsolved/title-case.js
+++ 4/03-title-case/Unsolved/title-case.js
@@ -1,5 +1,9 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {
-    
+var titleCase = function (str) {
+  console.log(str.split(" "));
+ return str.split(" ").map((word) => {
+    return word[0].toUpperCase()+ word.slice(1)
+  }).join(" ");
+ 
 };
