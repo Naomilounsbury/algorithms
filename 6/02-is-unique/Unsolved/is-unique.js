53b4c3faacdfbfd13e86834e9575d194313e7ec5
naomi <naomilounsbury@hotmail.com>
2022-05-16 21:28:10 -0700

    so confused on how I got that

--- 6/02-is-unique/Unsolved/is-unique.js
+++ 6/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,15 @@
 // TODO: Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function (arr) {
+  const numbers = {};
+  for (var i = 0; i <= arr.length; i++) {
+    const num = arr[i];
+    if (numbers[num]) {
+      return false;
+    }
+    //so I just played with this until I got it but I dont really understand it 
+    numbers[num] = true;
+  }
+  return true;
+};
