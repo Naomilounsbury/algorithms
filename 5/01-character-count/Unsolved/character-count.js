f5023d0d091902c30349f0a44e507689c318b3ab
naomi <naomilounsbury@hotmail.com>
2022-05-24 22:32:43 -0700

    product of largest number but I haven't got it working for negative numbers

--- 5/01-character-count/Unsolved/character-count.js
+++ 5/01-character-count/Unsolved/character-count.js
@@ -1,3 +1,11 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function (str) {
+  console.log(str);
+  let newObject = 0;
+  for (var i = 0; i < str.length; i++) {
+    const abc = /[abc]/;
+    const newString = str.split("");
+    console.log(newString);
+  }
+};
--- 5/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ 5/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,3 +1,17 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {};
+var productOfLargestTwo = function (arr) {
+  let largestNum = 0;
+  let secondLargest = 0;
+  arr.forEach((element, i) => {
+    if (element > largestNum) {
+      largestNum = element;
+    }
+  });
+  arr.forEach((element) => {
+    if (element > secondLargest && element < largestNum) {
+      secondLargest = element;
+    }
+  });
+  return largestNum * secondLargest;
+};
